5da20d3997e3572e42e7a3783342ef6a19a14919
apolloscript3587 <apolloscript3587@gmail.com>
2026-09-14 13:53:18 -0500

    Update contador_led_indicador.ino

--- codigo/contador_led_indicador.ino
+++ codigo/contador_led_indicador.ino
@@ -1,2 +1,31 @@
-// Contador binario con quinto LED indicador para el valor 15
-// TODO: agregar implementación
+const int leds[] = {9, 10, 11, 12};
+const int led_indicador = 13;
+
+int contador = 0;
+
+int tiempo_espera = 1000;
+
+void setup() {
+  for (int i = 0; i < 4; i += 1) {
+    pinMode(leds[i], OUTPUT);
+  }
+  
+  pinMode(led_indicador, OUTPUT);
+}
+
+void loop() {
+  for (int i = 0; i < 4; i += 1) {
+    int bit = (contador >> i) & 1;
+    digitalWrite(leds[i], bit);
+  }
+  
+  contador += 1;
+  
+  if (contador == 16) {
+    digitalWrite(led_indicador, HIGH);
+  } else {
+    digitalWrite(led_indicador, LOW);
+  }
+  
+  delay(tiempo_espera);
+}
