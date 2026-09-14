0cbec2a11e8c4518db3bae970cda7fe615194ec1
apolloscript3587 <apolloscript3587@gmail.com>
2026-09-14 13:09:04 -0500

    Update contador_descendente.ino

--- codigo/contador_descendente.ino
+++ codigo/contador_descendente.ino
@@ -1,2 +1,22 @@
-// Contador binario descendente de 4 bits
-// TODO: agregar implementación
+const int leds[] = {9, 10, 11, 12};
+
+int contador = 15;
+
+int tiempo_espera = 1000;
+
+void setup() {
+  for (int i = 0; i < 4; i += 1) {
+    pinMode(leds[i], OUTPUT);
+  }
+}
+
+void loop() {
+  for (int i = 0; i < 4; i += 1) {
+    int bit = (contador >> i) & 1;
+    digitalWrite(leds[i], bit);
+  }
+  
+  contador -= 1;
+  
+  delay(tiempo_espera);
+}
