cc0a829f8f6eea0b60a5fc25c1f56963e5ed1c10
apolloscript3587 <apolloscript3587@gmail.com>
2026-09-14 13:04:18 -0500

    Add files via upload

---
+++ codigo/contador_arduino_4_bits1 (1).ino
@@ -0,0 +1,22 @@
+const int leds[] = {9, 10, 11, 12};
+
+int contador = 0;
+
+int tiempo_espera = 500;
+
+void setup() {
+  for (int i = 0; i < 4; i += 1) {
+    pinMode(leds[i], OUTPUT);
+  }
+}
+
+void loop() {
+  for (int i = 0; i < 4; i += 1) {
+    int bit = (contador >> i) & 1;
+    digitalWrite(leds[i], bit);
+  }
+  
+  contador += 1;
+  
+  delay(tiempo_espera);
+}
