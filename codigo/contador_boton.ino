efb0ba152d60f17f3cd099401e32ec8d2dc88c97
apolloscript3587 <apolloscript3587@gmail.com>
2026-09-14 13:32:51 -0500

    Update contador_boton.ino

--- codigo/contador_boton.ino
+++ codigo/contador_boton.ino
@@ -1,2 +1,27 @@
-// Contador binario controlado mediante botón
-// TODO: agregar implementación
+const int leds[] = {9, 10, 11, 12};
+const int boton = 8;
+
+int contador = 0;
+
+int tiempo_espera = 200;
+
+void setup() {
+  for (int i = 0; i < 4; i += 1) {
+    pinMode(leds[i], OUTPUT);
+  }
+
+  pinMode(boton, INPUT_PULLUP);
+}
+
+void loop() {
+  for (int i = 0; i < 4; i += 1) {
+    int bit = (contador >> i) & 1;
+    digitalWrite(leds[i], bit);
+  }
+
+  if (digitalRead(boton) == LOW) {
+    contador += 1;
+
+    delay(tiempo_espera);
+  }
+}
