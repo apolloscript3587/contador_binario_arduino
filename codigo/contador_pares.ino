77184e47f7e3df748df6094604036a11489baab2
apolloscript3587 <apolloscript3587@gmail.com>
2026-09-14 13:18:08 -0500

    Update contador_pares.ino

--- codigo/contador_pares.ino
+++ codigo/contador_pares.ino
@@ -1,2 +1,22 @@
-// Contador binario que muestra solamente números pares
-// TODO: agregar implementación
+const int leds[] = {9, 10, 11, 12};
+
+int contador = 0;
+
+int tiempo_espera = 1000;
+
+void setup() {
+  for (int i = 0; i < 4; i += 1) {
+    pinMode(leds[i], OUTPUT);
+  }
+}
+
+void loop() {
+  for (int i = 0; i < 4; i += 1) {
+    int bit = (contador >> i) & 1;
+    digitalWrite(leds[i], bit);
+  }
+  
+  contador += 2;
+  
+  delay(tiempo_espera);
+}
